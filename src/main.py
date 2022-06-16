fb5e0604e8e7a6e55cd3c1fa9df8a8b9fc185f6c
Tanvesh Sarve <sarvetanvesh01@gmail.com>
2022-06-16 19:22:22 +0000

    change in main.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ from src.script import func, hello, special_characters_in_string, boo
 from time import time
 
 
-print(func(3, 5))
-print(func(2, 3))
+print(func(3, 7))
+print(func(2, 1))
 print(hello())
 print(special_characters_in_string())
 
