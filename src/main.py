32ec1b326f83b3e0ba5aea285d300107b9fd825f
Tanvesh Sarve <sarvetanvesh01@gmail.com>
2022-06-18 15:11:51 +0000

    test

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from src.script import func, hello, special_characters_in_string, boo
 from time import time
 
 
-print(func(3, 7))
+print(func(3, 10))
 print(func(2, 2))
 print(hello())
 print(special_characters_in_string())
